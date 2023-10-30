ca4e04c89b0458036c178a297756618c953ffff3
echeaves <139502640+echeaves@users.noreply.github.com>
2023-10-30 16:10:20 -0700

    File Upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let numbers = [2,4,6,8,10];
+let total = 0;
+
+for(let i = 0; i<numbers.length; i++){
+  total += numbers[i];
+  }
+console.log(`Array Total is ${total}.`);
+
+
+let arrayAverage = total/numbers.length
+
+console.log(`Array Average is ${arrayAverage}.`);
